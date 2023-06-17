237b56c23b5917e28c61a654cb66ba5038025de8
imkozin <ivan.kozin@hotmail.com>
2023-06-17 14:47:39 +0300

    added register form

--- myproject_root/myproject/urls.py
+++ myproject_root/myproject/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from register import views as v
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('register/', v.register, name='register'),
     path('news/', include('news.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
